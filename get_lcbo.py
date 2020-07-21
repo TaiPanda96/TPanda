1f1b6680c196900f02daff7604dd4c220042f8d0
TaiPanda96 <61389462+TaiPanda96@users.noreply.github.com>
2020-07-21 19:57:38 -0400

    Add files via upload

--- get_lcbo.py
+++ get_lcbo.py
@@ -4,19 +4,7 @@ import sys
 from urllib.request import Request, urlopen
 import requests
 import pandas as pd
-
-class URL_list:
-    def __init__(self,url):
-        self.url = []
-    
-    def create(self,url):
-        for url_strings in range(len(url)):
-            return self.url.append(url_strings)
-
-    def lcbo_data_response(self,url):
-        for url_strings in self.url:
-            page_obj = get_tags(url)
-        return page_obj
+from functools import reduce
 
 
 def get_html(url):
@@ -25,59 +13,65 @@ def get_html(url):
         {"User-Agent":"Mozilla/5.0"}
     )
     page = requests.get(url,headers=headers)
-    return page
+    data = BeautifulSoup(page.content,'html.parser')
+    return data
 
-def get_tags(url):
-    page = get_html(url)
-    product_list = []
+
+def get_tags(data):
+    products_ = []
     prices = []
 
-    if page.status_code == 200:
-        data = BeautifulSoup(page.content,'html.parser')
-        tag = data.a
-        
-        # lcbo_id     = data.find_all(attrs={'col-xs-7 product_info product-info-section'})
-        product_id  = data.find_all('div',attrs={'class':'product_name'})
-        # price_data  = data.find_all('div', attrs={'class':'product_price'})
-        offer_price = data.find_all('span', attrs={'price'})
-
-        # print(repr(offer_price))
+    try:
+        product_id    = data.find_all('div',attrs={'class':'product_name'})
+        offer_price   = data.find_all('span', attrs={'price'})
 
         for products in product_id:
-            prod_elems = products.text
-            prod_clean = prod_elems.strip('\n')
-            product_list.append(prod_clean)
+            prod_clean = products.text.strip('\n')
+            products_.append(prod_clean)
         
         for data in offer_price:
             elems = repr(data.text)
             for elems in data:
                 prices.append(elems.translate({ord(i): None for i in '\n\t\t\t'}))
     
+        return convert(products_,prices)
+    except:
+        print(sys.exc_info()[0], sys.exc_info()[1])
 
-        items_map = {
-            'Product' : {i: product_list[i] for i in range(1,len(product_list))},
-            'Price'   : {i: prices[i] for i in range(len(prices))},
-            }
 
-        return items_map
-    else:
-        print('The HTTP response has the following code -->',page.status_code,sys.exc_info()[0], sys.exc_info()[1])
-       
+def convert(products_,prices):
+    items_map = {
+        'Product' : {i: products_[i] for i in range(1,len(products_))},
+        'Price'   : {i: prices[i] for i in range(len(prices))},
+        }
+    return items_map
 
 def table(items_map):
     data = pd.DataFrame().from_dict(data=items_map,orient='columns')
-
     prod_data  = data.iloc[1:,0]
     price_data = data.iloc[:,1].shift(1)
-
     result = pd.concat([prod_data,price_data],axis=1)
     beers = result.drop([0],axis=0)
-    print(beers)
+    return beers.dropna()
+
+def retrieve_promos(url):
+    data               = get_html(url)
+    items_map          = get_tags(data)
+    items_table        = table(items_map)
+    return items_table
 
 
-
-if __name__ == "__main__":
-    url = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/beer-cider-16'
-    items_map = get_tags(url)
-    table(items_map)
+# if __name__ == "__main__":
+#     url  = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/beer-cider-16'
+#     url2 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15'
+#     url2 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/spirits-15'
+#     url3 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/beer-cider-16/lager-16023'
+#     url4 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/coolers-18-1'
+#     url5 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/beer-cider-16/hybrid-16024'
+#     url6 = 'https://www.lcbo.com/webapp/wcs/stores/servlet/en/lcbo/beer-cider-16/lager-16023/flavoured-beer-16023306-1'
+#     url_list = [url,url2,url3]
+#     for urls in url_list:
+#         items = retrieve_promos(urls)
+#         print(items)
+ 
     
